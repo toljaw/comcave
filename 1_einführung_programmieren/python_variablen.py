b5ed3099d0ca03cad69962931e4880a04efb3d40
AWEI177 <toljaw@gmx.de>
2024-02-13 12:18:49 +0100

    update

---
+++ 1_einführung_programmieren/python_variablen.py
@@ -0,0 +1,24 @@
+# das ist ein Kommentar
+
+a = 100
+print(f"Der Wert 'a' ist {a}.")
+
+n = 0xab3
+print(f"n: {n}")
+
+N = 0b1010
+print(f"N: {N}")
+
+# Oktalzahl:
+oct_num = 0o10
+print(f"Die Oktalzahl 0o10: {oct_num}")
+
+print(f"der Typ von a ist: {type(a)}")
+print(f"der Typ von n ist: {type(n)}")
+print(f"der Typ von N ist: {type(N)}")
+print(f"der Typ von oct_num ist: {type(oct_num)}")
+
+#Binärzahl an int übergeben:
+n1 = int("110101", 2)
+
+print(n1)
