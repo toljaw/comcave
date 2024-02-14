73e7314ab3530e154165b383d3fd72185ebb2c55
Anatoli Weikum <weikum.anatoli@gmail.com>
2024-02-14 12:44:51 +0100

    ...

--- 1_einführung_programmieren/test.py
+++ 1_einführung_programmieren/test.py
@@ -1,4 +1,25 @@
+'''
 import random
 studentList = ["Muhamed", "Kirill", "Fabio", "Arijan", "Peter", "Masuda", "Anatoli"]
 randomStudent = studentList[random.randint(0, 6)]
 print(randomStudent)
+'''
+
+liste1 = [1,2,2,3,4,5]
+
+liste1.append(34)
+liste1.append(False)
+liste1.append("STARK")
+
+dict1 = {
+    "Fabio": "ok",
+    "Kirill": "ok"
+}
+
+print(liste1)
+print(dict1)
+
+ersterInput = input("Was geht? ")
+
+print(ersterInput)
+print(dict1.get("Fabio"))
--- 1_einführung_programmieren/tester.py
+++ 1_einführung_programmieren/tester.py
@@ -4,4 +4,5 @@ nummer2 = 5
 summe1 = nummer1 * nummer2
 
 print(summe1)
+
 print(f"Hallo das Ergebniss ist {summe1}")
