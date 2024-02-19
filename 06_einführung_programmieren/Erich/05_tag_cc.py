27dbd0b6a46c7b7ed4218b307358c54a721fdb68
Anatoli Weikum <weikum.anatoli@gmail.com>
2024-02-19 16:51:12 +0100

    add code challenge for day 4, 5, 6

---
+++ 06_einführung_programmieren/Erich/05_tag_cc.py
@@ -0,0 +1,54 @@
+'''
+511
+Gib alle ganzen Zahlen von 1 bis 10 aus.
+'''
+
+
+
+'''
+512
+Gib alle Dezimalzahlen zwischen 0 und 1 aus mit einer Schrittweite von 0.1 .
+'''
+
+'''
+513
+Gib jede dritte Zahl zwischen 1 und 20 aus. 
+'''
+
+'''
+514
+Gib Zahlen  zwischen -30 und 30 aus. Es soll aber nur jede vierte Zahl sein. 
+Ausgabe: 
+
+-30 -26 -22 -18 -14 -10 -6 -2 2 6 10 14 18 22 26 30 
+
+Hinweis: Ausgabe hintereinander: 
+
+print ( ….. , end=" ") # Leerzeichen als String hinter end= 
+'''
+
+'''
+515
+Betrachte alle Zahlen zwischen 1 und 100 (1 und 100 eingeschlossen). 
+Ist eine Zahl durch 8 teilbar, so gib diese Zahl aus. 
+Ist eine Zahl durch 12 teilbar, so gib diese Zahl aus. 
+
+Hinweis: Ausgabe hintereinander: 
+
+print ( ….. , end=" ") # Leerzeichen als String hinter end= 
+'''
+
+'''
+521
+Zähle einen Wert mit ganzen Zahlen von 1 bis 200 aufwärts. Gib dafür eine Schrittweite ein oder setze eine Variable. Benutze keine for-Anweisung. 
+'''
+
+'''
+522
+Bestimme eine Schrittweite. Zähle einen Wert mit ganzen Zahlen von 0 aufwärts bis 199. Ist der Wert 199 erreicht oder überschritten, so zähle den Wert mit der Schrittweite wieder abwärts bis 0 oder weniger erreicht ist. 
+'''
+
+'''
+523
+Mache den Code aus CC-522 zu einer Endlosschleife. (Abbruch ist dann mit CTRL-C möglich!) 
+'''
