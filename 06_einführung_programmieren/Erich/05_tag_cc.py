68e5aeb407933e76ae234a6997960ee552fbe62b
Anatoli Weikum <weikum.anatoli@gmail.com>
2024-02-19 18:05:10 +0100

    finalize cc day 4 and 5

--- 06_einführung_programmieren/Erich/05_tag_cc.py
+++ 06_einführung_programmieren/Erich/05_tag_cc.py
@@ -2,18 +2,25 @@
 511
 Gib alle ganzen Zahlen von 1 bis 10 aus.
 '''
-
+# for zahl in range(1,11):
+#     print(zahl)
 
 
 '''
 512
 Gib alle Dezimalzahlen zwischen 0 und 1 aus mit einer Schrittweite von 0.1 .
 '''
+# for zahl in range(0,11):
+#     print(zahl/10)
+
 
 '''
 513
 Gib jede dritte Zahl zwischen 1 und 20 aus. 
 '''
+# for zahl in range(3,21, 3):
+#     print(zahl)
+
 
 '''
 514
@@ -26,6 +33,9 @@ Hinweis: Ausgabe hintereinander:
 
 print ( ….. , end=" ") # Leerzeichen als String hinter end= 
 '''
+# for zahl in range(-30,31,4):
+#     print(zahl, end=" ")
+
 
 '''
 515
@@ -37,18 +47,57 @@ Hinweis: Ausgabe hintereinander:
 
 print ( ….. , end=" ") # Leerzeichen als String hinter end= 
 '''
+# for zahl in range(1,101):
+#     if zahl % 8 == 0 or zahl % 12 == 0:
+#         print(zahl, end=" ")
+
 
 '''
 521
 Zähle einen Wert mit ganzen Zahlen von 1 bis 200 aufwärts. Gib dafür eine Schrittweite ein oder setze eine Variable. Benutze keine for-Anweisung. 
 '''
+# schrittweite = 10
+# zahl = 1
+# zählen = True
+
+# while zählen:
+#     print(zahl)
+#     zahl += schrittweite
+#     if zahl > 200:
+#         zählen = False
+
 
 '''
 522
 Bestimme eine Schrittweite. Zähle einen Wert mit ganzen Zahlen von 0 aufwärts bis 199. Ist der Wert 199 erreicht oder überschritten, so zähle den Wert mit der Schrittweite wieder abwärts bis 0 oder weniger erreicht ist. 
 '''
+# schrittweite = 11
+# zahl = 0
+# zählen = True
+
+# while zählen:
+#     print(zahl)
+#     zahl += schrittweite
+#     if zahl >= 199:
+#         schrittweite *= -1
+#     if zahl <= 0:
+#         print(zahl)
+#         zählen = False
+
 
 '''
 523
 Mache den Code aus CC-522 zu einer Endlosschleife. (Abbruch ist dann mit CTRL-C möglich!) 
 '''
+# schrittweite = 11
+# zahl = 0
+# zählen = True
+
+# while zählen:
+#     print(zahl)
+#     zahl += schrittweite
+#     if zahl >= 199:
+#         schrittweite *= -1
+#     if zahl <= 0:
+#         print(zahl)
+#         #zählen = False
--- 06_einführung_programmieren/Erich/06_tag_aufgaben.py
+++ 06_einführung_programmieren/Erich/06_tag_aufgaben.py
@@ -1,18 +1,80 @@
+"""
+1
 #Definiere eine Funktion zur Berechnung des Mittelwertes zweier Zahlen. 
+"""
+
+# n1 = 14 
+# n2 = 20 
+
+# def  mittel(a,b):
+#     ergebnis = (a+b)/2
+#     return ergebnis 
 
 
-n1 = 14 
+# print (mittel(n1,n2)) 
 
-n2 = 20 
+"""
+2
+Schau dir Aufgabe 6 von Tag 5 an und Aufgabe 7. 
+Du findest dort 3 Varianten zur Summenberechnung. 
 
- 
+Erstelle drei Funktionen  sumA (kopfgesteuert), sumB(fußgesteuert), sumC (Zählschleife). 
 
-def  mittel(a,b):
-    ergebnis = (a+b)/2
-    return ergebnis 
+Eingabe jeder Funktion :  die ganze Zahl n 
+
+Ausgabe jeder Funktion: die Summe von 1 bis n 
+
+so sieht dein main-Abschnitt aus: 
+
+n= 100 
+
+# Definition der verschiedenen Funktionen 
+
+# main - Hauptcode 
+
+print ('kopfgesteuert:', sumA (n) ) 
+
+print ('fußgesteuert:', sumB (n) ) 
+
+print ('Zählschleife:', sumC (n) ) 
+
+Hinweis: du kannst die obigen Code-Zeilen rauskopieren….in deine  IDE 
+"""
 
 
-print (mittel(n1,n2)) 
+"""
+3
+Welche der Varianten aus Aufgabe 2 ist am schnellsten? 
+Wie kannst du das ermitteln ? 
+Hinweis: Zeitmessung….. 
 
- 
-#Fülle die Funktion aus… 
+Vorlage des PythonCodes:: 
+
+import time 
+
+n=5000   # Summe 1 bis 5000  
+
+dummy =0 
+
+def sumA (n): 
+    summe = 0 
+    hilfszahl=1 
+
+    while hilfszahl <= n: 
+        summe += hilfszahl 
+        hilfszahl +=1 
+    return summe 
+
+startA = time.time() 
+
+for j in range (10000):     # 10000 mal Berechnung wiederholen 
+    dummy = sumA(n) 
+
+endA = time.time() 
+dauerA = endA - startA 
+print ('Variante A:', dauerA, ' sec') 
+
+Erweitere die Zeitmessung um sumB() und um SumC(). 
+
+Welche Variante ist am schnellsten? 
+"""
