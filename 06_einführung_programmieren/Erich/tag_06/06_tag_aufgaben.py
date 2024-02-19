68e5e16015c842df391434d41e3ff4198410e357
Anatoli Weikum <weikum.anatoli@gmail.com>
2024-02-19 16:29:28 +0100

    change file structure - improve usability

---
+++ 06_einführung_programmieren/Erich/tag_06/06_tag_aufgaben.py
@@ -0,0 +1,18 @@
+#Definiere eine Funktion zur Berechnung des Mittelwertes zweier Zahlen. 
+
+
+n1 = 14 
+
+n2 = 20 
+
+ 
+
+def  mittel(a,b):
+    ergebnis = (a+b)/2
+    return ergebnis 
+
+
+print (mittel(n1,n2)) 
+
+ 
+#Fülle die Funktion aus… 
